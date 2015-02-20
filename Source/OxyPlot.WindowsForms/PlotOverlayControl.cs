cded4178a6018a72a955e94ae870f811675ac2c7
User <User@mail.com>
2015-02-20 14:48:19 -0500

    WindowsForms: Add plot overlay control

---
+++ Source/OxyPlot.WindowsForms/PlotOverlayControl.cs
@@ -0,0 +1,61 @@
+﻿
+namespace OxyPlot.WindowsForms
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Base class for creating plot overlays.
+    /// </summary>
+    [Serializable]
+    public abstract class PlotOverlayControl : Control
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlotOverlayControl"/> class.
+        /// </summary>
+        protected PlotOverlayControl()
+        {
+            // ReSharper disable once DoNotCallOverridableMethodsInConstructor
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            // ReSharper disable once DoNotCallOverridableMethodsInConstructor
+            this.BackColor = Color.Transparent;
+        }
+
+        /// <summary>
+        /// Render the overlay.
+        /// </summary>
+        /// <param name="e"></param>
+        public void Render(PaintEventArgs e)
+        {
+            //this.OnPaintBackground(e);
+            this.OnPaint(e);
+        }
+
+        /// <summary>
+        /// Processes Windows messages.
+        /// <remarks>
+        /// Pass mouse events through to parent control.
+        /// https://stackoverflow.com/questions/547172/pass-through-mouse-events-to-parent-control
+        /// </remarks>
+        /// </summary>
+        /// <param name="m">The Windows System.Windows.Forms.Message to process.</param>
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NCHITTEST = 0x0084;
+            const int HTTRANSPARENT = (-1);
+
+            if (m.Msg == WM_NCHITTEST)
+            {
+                m.Result = (IntPtr)HTTRANSPARENT;
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
---
+++ Source/OxyPlot.WindowsForms/PlotOverlayGroupControl.cs
@@ -0,0 +1,117 @@
+﻿
+namespace OxyPlot.WindowsForms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// A control that renders <see cref="PlotOverlayControl"/> objects over the <see cref="OxyPlot.WindowsForms.PlotView"/>.
+    /// </summary>
+    [Serializable]
+    public class PlotOverlayGroupControl : Control
+    {
+
+        /// <summary>
+        /// Overlay controls.
+        /// </summary>
+        //private readonly List<PlotOverlayControl> _overlayControls;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlotOverlayGroupControl"/> class.
+        /// </summary>
+        public PlotOverlayGroupControl()
+        {
+            // ReSharper disable once DoNotCallOverridableMethodsInConstructor
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            // ReSharper disable once DoNotCallOverridableMethodsInConstructor
+            BackColor = Color.Transparent;
+
+            //_overlayControls = new List<PlotOverlayControl>();
+        }
+
+        /// <summary>
+        /// Gets the overlay controls.
+        /// </summary>
+        //public List<PlotOverlayControl> OverlayControls
+        //{
+        //    get { return _overlayControls; }
+        //}
+
+        /// <summary>
+        /// Raises the System.Windows.Forms.Control.Paint event.
+        /// </summary>
+        /// <param name="e">A System.Windows.Forms.PaintEventArgs that contains the event data.</param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            try
+            {
+                //foreach (PlotOverlayControl overlay in _overlayControls)
+                foreach (PlotOverlayControl overlay in this.Controls)
+                {
+                    if (!overlay.Visible)
+                    {
+                        continue;
+                    }
+                    overlay.Render(e);
+                }
+            }
+            catch (Exception paintException)
+            {
+                var trace = new StackTrace(paintException);
+                Debug.WriteLine(paintException);
+                Debug.WriteLine(trace);
+                using (var font = new Font("Arial", 10))
+                {
+                    e.Graphics.ResetTransform();
+                    e.Graphics.DrawString(
+                        "PlotOverlayGroupControl paint exception: " + paintException.Message, font, Brushes.Red,
+                        Width * 0.5f,
+                        Height * 0.5f,
+                        new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                }
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            //foreach (var item in this._overlayControls)
+            foreach (Control item in this.Controls)
+            {
+                item.Size = this.Size;
+            }
+        }
+
+        /// <summary>
+        /// Processes Windows messages.
+        /// <remarks>
+        /// Pass mouse events through to parent control.
+        /// https://stackoverflow.com/questions/547172/pass-through-mouse-events-to-parent-control
+        /// </remarks>
+        /// </summary>
+        /// <param name="m">The Windows System.Windows.Forms.Message to process.</param>
+        protected override void WndProc(ref Message m)
+        {
+            // ReSharper disable once InconsistentNaming
+            const int WM_NCHITTEST = 0x0084;
+            // ReSharper disable once InconsistentNaming
+            const int HTTRANSPARENT = (-1);
+
+            if (m.Msg == WM_NCHITTEST)
+            {
+                m.Result = (IntPtr)HTTRANSPARENT;
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
